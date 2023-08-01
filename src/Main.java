6ba1d1cf3907a21ba10c0f1e0f8e0d0680e39b37
Ruslan Popov <ruslan_popov_2014@inbox.ru>
2023-08-01 20:54:41 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Person ivan = new Person(1, "Ivan", 10_000, "Men");
+        Person elena = new Person(2, "Elena", 12_000, "Women");
+        Person petr = new Person(3, "Petr", 11_000, "Men");
+
+        ConcurrentHashMap<Integer, Person> map = new ConcurrentHashMap<>();
+        map.put(ivan.getId(), ivan);
+        map.put(elena.getId(), elena);
+        map.put(petr.getId(), petr);
+
+        // System.out.println(map.get(1));
+
+
+        Thread thread = new Thread(() -> readMap(map));
+        thread.start();
+
+
+    }
+
+    public static void readMap(Map<Integer, Person> map) {
+        for (Map.Entry<Integer, Person> entry : map.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println();
+
+        map.put(1, new Person(4, "Olga", 15_000, "Women"));
+        map.put(2, new Person(5, "Oleg", 14_000, "Men"));
+        map.put(3, new Person(6, "Jon", 9_000, "Men"));
+        System.out.println(map.get(1));
+        System.out.println(map.get(2));
+        System.out.println(map.get(3));
     }
 }
+
+
+
--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,61 @@
-public class Map {
+import java.util.HashMap;
+import java.util.Map;
 
-private int id;
-private String name;
+public class Person {
 
-private long p
-private String gender;
+    private int id;
+    private String name;
+    private long payment_amount;
+    private String gender;
 
+
+    public Person(int id, String name, long payment_amount, String gender) {
+        this.id = id;
+        this.name = name;
+        this.payment_amount = payment_amount;
+        this.gender = gender;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPayment_amount() {
+        return payment_amount;
+    }
+
+    public void setPayment_amount(long payment_amount) {
+        this.payment_amount = payment_amount;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", payment_amount=" + payment_amount +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
 }
